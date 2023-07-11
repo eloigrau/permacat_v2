8c8e917305949c8512f97a8ebc1807ca00279d45
eloigra <eloi.grau@gmail.com>
2023-07-11 13:42:21 +0200

    Jardins partagés + adresse picker +...

--- bourseLibre/static/js/bourseLibre.js
+++ bourseLibre/static/js/bourseLibre.js
@@ -125,13 +125,13 @@ document.addEventListener("DOMContentLoaded", function(){
 //});
 
 
-(function copyToClipboard(text) {
+function copyToClipboard(text) {
     navigator.clipboard.writeText(text).then(function() {
       console.log('Async: Copying to clipboard was successful!');
     }, function(err) {
       console.error('Async: Could not copy text: ', err);
     });
-})
+}
 
 
 function toggle_visibility(id) {

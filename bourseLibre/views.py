20a14e4efa4caeb1c08a26dfcf090346abd10751
eloigra <eloi.grau@gmail.com>
2023-10-23 15:10:31 +0200

    nettoyage static

--- bourseLibre/views.py
+++ bourseLibre/views.py
@@ -1219,9 +1219,10 @@ def salon_accueil(request):
     suivis, created = Suivis.objects.get_or_create(nom_suivi="salon_accueil")
     invit = InvitationDansSalon.objects.filter(profil_invite=request.user).order_by("-date_creation")
     inner_qs = list(set(list(salons_inscrit.values_list('salon__tags', flat=True)) +
-                        list( salons_publics.values_list('tags').distinct())))
+                        list( salons_publics.values_list('tags', flat=True).distinct())))
     inner_qs.remove(None)
-    inner_qs.remove((None, ))
+    if (None, ) in inner_qs:
+        inner_qs.remove((None, ))
     tags = Tag.objects.filter(id__in=inner_qs)
 
     return render(request, 'salon/accueilSalons.html', {'salons_prives':salons_prives, "salons_publics":salons_publics, "salons_recents":salons_recents, "salons_su":salons_su, "invit":invit, "suivis":suivis, "tags":tags })

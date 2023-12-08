8a1bf47e9f718d12fc218f0c4fe8865b392ff094
eloigra <eloi.grau@gmail.com>
2023-12-08 14:52:35 +0100

    listes diffusions adherents

--- script_update_rapide.sh
+++ script_update_rapide.sh
@@ -3,7 +3,7 @@ cd /home/udjango/permacat
 git pull
 source /home/udjango/permacat/permacatenv/bin/activate
 python manage.py migrate --settings=bourseLibre.settings.production
-python manage.py crontab add
+python manage.py crontab add --settings=bourseLibre.settings.production
 sudo systemctl restart nginx
 sudo supervisorctl restart permacat_supervisor
 

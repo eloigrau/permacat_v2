ea11f861eae252d09c5ed2b7f8e9a4daafcc42c9
eloigra <eloi.grau@gmail.com>
2024-04-03 21:43:30 +0200

    listes diffusions adherents

--- bourseLibre/views_notifications.py
+++ bourseLibre/views_notifications.py
@@ -226,18 +226,18 @@ def notifications_news_regroup(request):
                 if action.description.startswith("a ajouté un article "):
                     htmlArticles += "créé par "+ str(action.actor)
                 elif action.description.startswith("a ajouté une date"):
-                    htmlArticles += str(action.actor) + "a ajouté une date"
+                    htmlArticles += str(action.actor) + " a ajouté une date"
                 elif action.description.startswith("a ajouté un lieu"):
-                    htmlArticles += str(action.actor) + "a ajouté un lieu"
+                    htmlArticles += str(action.actor) + " a ajouté un lieu"
                 elif action.description.startswith("a ajouté un document partagé"):
-                    htmlArticles += str(action.actor) + "a ajouté undocument partagé"
+                    htmlArticles += str(action.actor) + " a ajouté undocument partagé"
 
-                elif action.description.startswith("a ajouté le document"):
+                elif action.description.startswith(" a ajouté le document"):
                     htmlArticles += str(action.actor) + str(action.description)
                 elif action.description.startswith("a ajouté un salon"):
-                    htmlArticles += str(action.actor) + "a ajouté un salon"
+                    htmlArticles += str(action.actor) + " a ajouté un salon"
                 elif action.description.startswith("a ajouté une réunion"):
-                    htmlArticles += str(action.actor) + "ajout d'une réunion"
+                    htmlArticles += str(action.actor) + "a ajouté une réunion"
             elif action.description.startswith("a modif"):
                 htmlArticles += "modifié par "+ str(action.actor)
             elif action.description.startswith("a archiv"):

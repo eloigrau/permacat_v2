5aa54af894fc2ebe0d07a0d92b909a085127cef0
eloigra <eloi.grau@gmail.com>
2024-04-12 10:56:32 +0200

    listes diffusions adherents

--- defraiement/models.py
+++ defraiement/models.py
@@ -17,7 +17,8 @@ class Choix:
     statut_projet = ('prop','Proposition de projet'), ("AGO","Projet soumise à l'AGO"), ('accep',"Accepté par l'association"), ('refus',"Refusé par l'association" ),
     type_reunion_asso = {
         "rtg": ["Réunion équipe", 'Troc de Graine', 'Atelier', 'Rencontre', 'FestiGraines', 'Visite de Jardin', 'Autre'],
-        "scic": ['Cercle Ancrage', 'Cercle thématique', 'Cercle Education', 'Cercle Jardins', 'Evenement', 'Divers',]
+        "scic": ['Cercle Ancrage', 'Cercle thématique', 'Cercle Education', 'Cercle Jardins', 'Evenement', 'Divers',],
+        "conf66": ['Réunion CS', 'Evenement', 'Manifestation']
       }
 
     type_reunion = [(str(i), y) for i, y in enumerate([x for x in list(itertools.chain.from_iterable(type_reunion_asso.values()))])]

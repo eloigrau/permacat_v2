bdbdf5bb1dc0ea98a28289e9ccc5c506250f4f0c
eloigra <eloi.grau@gmail.com>
2024-06-16 15:31:01 +0200

    listes diffusions adherents

--- bourseLibre/static/js/bourseLibre.js
+++ bourseLibre/static/js/bourseLibre.js
@@ -155,3 +155,33 @@ function scrollToMe(element) {
     location.href = element;
 }
 
+function geoFindMe() {
+  const status = document.querySelector("#status");
+  const mapLink = document.querySelector("#map-link");
+
+  mapLink.href = "";
+  mapLink.textContent = "";
+
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = "";
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = "Impossible de retrouver votre position";
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocalisation indisponible dans votre navigateur";
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+
+/* document.querySelector("#find-me").addEventListener("click", geoFindMe);*/

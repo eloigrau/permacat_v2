5fa9a4c6b792cc374051f6eeb8879780e2e20bef
eloigra <eloi.grau@gmail.com>
2024-07-17 11:34:53 +0200

    listes diffusions adherents

--- ateliers/migrations/0018_auto_20210312_0158.py
+++ ateliers/migrations/0018_auto_20210312_0158.py
@@ -14,11 +14,11 @@ class Migration(migrations.Migration):
             model_name='atelier',
             name='outils',
         ),
-        migrations.AlterField(
-            model_name='atelier',
-            name='heure_atelier',
-            field=models.TimeField(blank=True, default='17:00', help_text='Horaire de départ (hh:mm)', null=True, verbose_name='Heure prévue'),
-        ),
+        # migrations.AlterField(
+        #     model_name='atelier',
+        #     name='heure_atelier',
+        #     field=models.TimeField(blank=True, default='17:00', help_text='Horaire de départ (hh:mm)', null=True, verbose_name='Heure prévue'),
+        # ),
         migrations.AlterField(
             model_name='atelier',
             name='materiel',

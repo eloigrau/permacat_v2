4a0e92ebd0118bb5d417a523ae1df651c8cbd00d
eloigra <eloi.grau@gmail.com>
2024-07-20 23:13:23 +0200

    Passage à Django 4

--- script_update.sh
+++ script_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/udjango/permacat
 git pull
-source /home/udjango/permacat/permacatenv37/bin/activate
-pip install -r requirements_2.txt
+source /home/udjango/permacat/permacatenv39/bin/activate
+pip install -r requirements.txt
 python manage.py migrate --settings=bourseLibre.settings.production
 python manage.py collectstatic --noinput --settings=bourseLibre.settings.production
 python manage.py crontab add --settings=bourseLibre.settings.production

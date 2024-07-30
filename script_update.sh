f9c7fd9720ae1bd626dde0a666f7090dc2bbb68c
eloigra <eloi.grau@gmail.com>
2024-07-30 14:15:53 +0200

    Passage à Django 4 : suppression djanog filters

--- script_update.sh
+++ script_update.sh
@@ -2,7 +2,7 @@
 cd /home/udjango/permacat
 git pull
 source /home/udjango/permacat/permacatenv39/bin/activate
-pip install -r reqnov.txt
+pip install -r requirements.txt
 python manage.py migrate --settings=bourseLibre.settings.production
 python manage.py collectstatic --noinput --settings=bourseLibre.settings.production
 python manage.py crontab add --settings=bourseLibre.settings.production

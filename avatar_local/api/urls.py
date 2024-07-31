e389887fdf0143a1a998d9135d37af9675c14ac9
eloigra <eloi.grau@gmail.com>
2024-07-31 17:35:48 +0200

    Django 4.2.14 OK

--- avatar_local/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from avatar_local.api.views import AvatarViewSets
-
-router = SimpleRouter()
-router.register("avatar", AvatarViewSets)
-
-urlpatterns = router.urls

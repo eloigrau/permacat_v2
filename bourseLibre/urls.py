01534057ad007487bb5c90a9011eab414b4d2cf8
eloigra <eloi.grau@gmail.com>
2024-08-01 10:41:47 +0200

    Django 4.2.14 OK

--- bourseLibre/urls.py
+++ bourseLibre/urls.py
@@ -277,7 +277,7 @@ urlpatterns += [
 from django.conf import settings
 if settings.DEBUG:
     from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 else:
     urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 

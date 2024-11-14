efcd1db8fa0293aab9e8301ec061057ffcb0b5c2
eloi <eloi.grau@gmail.com>
2024-11-14 22:50:38 +0100

    ajout Comm_adh conf

--- bourseLibre/views_admin.py
+++ bourseLibre/views_admin.py
@@ -763,7 +763,6 @@ def recalculerAdressesConf(request):
         return HttpResponseForbidden()
     from adherents.models import Adherent
     add = Adherent.objects.all()
-
     message = ""
     for a in add:
         if a.latitude == LATITUDE_DEFAUT:

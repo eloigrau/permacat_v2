90746a9a3f1553343ba6c6097ec5fd756dd53126
eloi <eloi.grau@gmail.com>
2024-12-18 00:15:01 +0100

    ajout phonebook

--- bourseLibre/static/js/service-worker.js
+++ bourseLibre/static/js/service-worker.js
@@ -13,3 +13,23 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
+
+// Register event listener for the 'push' event.
+self.addEventListener('push', function (event) {
+    // Retrieve the textual payload from event.data (a PushMessageData object).
+    // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+    // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+    const eventInfo = event.data.text();
+    const data = JSON.parse(eventInfo);
+    const head = data.head || 'Nouvelle Notification';
+    const body = data.body || 'Message mystérieux';
+
+    // Keep the service worker alive until the notification is created.
+    event.waitUntil(
+        // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+        // as the body.
+        self.registration.showNotification(head, {
+            body: body,œ
+        })
+    );
+});

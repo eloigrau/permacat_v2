180ad85d010f571307aabc98d4936e131ea91cb2
eloi <eloi.grau@gmail.com>
2024-12-23 01:50:42 +0100

    ajout phonebook

--- bourseLibre/static/js/service-worker.js
+++ bourseLibre/static/js/service-worker.js
@@ -1,7 +1,18 @@
 
-
-
-
+// when the browser fetches a url
+self.addEventListener('fetch', function(event) {
+    // either respond with the cached object or go ahead and fetch the actual url
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
 
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {

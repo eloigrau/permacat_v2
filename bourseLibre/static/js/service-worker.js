f543e8ce4e5046482d2180f73136fbc7af087716
eloi <eloi.grau@gmail.com>
2024-12-28 22:41:45 +0100

    ajout phonebook

--- bourseLibre/static/js/service-worker.js
+++ bourseLibre/static/js/service-worker.js
@@ -62,15 +62,54 @@ self.addEventListener('push', function(event) {
 });
 
 
+//user clicked / tapped a push notification
 self.addEventListener('notificationclick', function(event) {
-    if (Notification.prototype.hasOwnProperty('data')) {
-        var url = event.notification.data.url;
-        event.waitUntil(clients.openWindow(url));
-    } else {
-      event.waitUntil(
-        event.preventDefault(),
-        event.notification.close(),
-        self.clients.openWindow('/')
-      );
-    }
+    const clickedNotification = event.notification;
+    clickedNotification.close();
+
+    //exit if the url could not be found
+    if (!event.notification.data || !event.notification.data.url) return;
+
+    //get url from event
+    var url = event.notification.data.url;
+    //if the url contains a #, remove it and everything after it
+    var cleanedUrl = url.indexOf('#') ? url.substring(0, url.indexOf('#')) :url;
+
+    event.waitUntil(
+        self.clients.matchAll({type: 'window', includeUncontrolled: true}).then( windowClients => {
+            console.log('opening window', windowClients.length, 'windows')
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+
+                //if the page url contains a #, remove it and everything after it
+                var cleanedClientUrl;
+                if (client.url.indexOf('#') !== -1)
+                    cleanedClientUrl = client.url.substring(0, client.url.indexOf('#'));
+                else cleanedClientUrl = client.url;
+
+                // if the cleaned URLs match
+                if (cleanedClientUrl === cleanedUrl && 'focus' in client) {
+                    //focus and reload the window that has this page open
+                    client.focus();
+
+                    //if the url had a # in it, first navigate to the cleaned url (otherwise it wont refresh)
+                    if (url.indexOf('#')) {
+                      return client.navigate(cleanedUrl)
+                      .then(() => client.navigate(url));
+                    } else {
+                      return client.navigate(url);
+                    }
+
+                    client.navigate(url);
+
+                    return;
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (self.clients.openWindow) {
+                return self.clients.openWindow(url);
+            }
+        })
+    );
 });
--- webpush/templates/webpush_serviceworker.js
+++ webpush/templates/webpush_serviceworker.js
@@ -1,16 +1,9 @@
-const offlineFallbackPage = "bienvenue.html";
+
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
 });
 
 self.addEventListener('fetch', (event) => {

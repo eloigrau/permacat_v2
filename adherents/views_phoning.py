d04f99dad4dc4f32452799ee0d3565db44366d80
eloi.grau@gmail.com <eloi.grau@gmail.com>
2025-06-20 15:32:55 +0200

    maj interface

--- adherents/views_phoning.py
+++ adherents/views_phoning.py
@@ -101,6 +101,8 @@ class Contact_modifier(UserPassesTestMixin, UpdateView, ):
         #action.send(self.request.user, verb='jardins_nouveau_jp', action_object=self.object, url=self.object.get_absolute_url(),
         #             description="a ajouté le Jardin: '%s'" % self.object.titre)
 
+        if "next" in self.request.GET:
+            return redirect(self.request.GET["next"])
         return redirect("adherents:phoning_projet_courant", asso_slug=self.asso.slug)
     # def form_valid(self, form):
     #     desc = " a modifié l'adhérent : " + str(self.object.nom) + ", " + str(self.object.prenom)+ " (" + str(

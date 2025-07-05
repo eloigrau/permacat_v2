0e72c7c127093c033a522508dfdb05becf1c9f83
eloi <eloi.grau@gmail.com>
2025-07-05 16:13:29 +0200

    ajout phonebook

--- bourseLibre/models.py
+++ bourseLibre/models.py
@@ -1328,7 +1328,6 @@ class Salon(models.Model):
     def est_membre(self, user):
         return user in self.membres.all()
 
-
     def getInscrits(self):
         return [u.profil for u in InscritSalon.objects.filter(salon=self)]
 

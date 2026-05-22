3b8cdcf8a9ef86fa0ec42e8f206eb63fd927615f
eloi.grau@gmail.com <eloi.grau@gmail.com>
2026-05-22 08:18:54 +0200

    permagora presentation

--- collectifssa/admin.py
+++ collectifssa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Message_collectifssa, InscriptionCLA
+from .models import Message_collectifssa, InscriptionCLA, Covoit
 
 # Register your models here.
 admin.site.register(Message_collectifssa)
 admin.site.register(InscriptionCLA)
+admin.site.register(Covoit)

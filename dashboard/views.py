fd040065c44f4bf6122d47c02f106f4ea606ece7
eloi.grau@gmail.com <eloi.grau@gmail.com>
2026-05-31 01:18:36 +0200

    permagora presentation

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,6 +13,7 @@ class DashboardView(TemplateView):
     # Predefined function
     def get_context_data(self, **kwargs):
         # A function to init the global layout. It is defined in web_project/__init__.py file
+        self.request.session["asso_slug"] = "ssa"
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
 
         return context
